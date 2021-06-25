d76e2e3f8a6a677673e15afd68933d404c99ae68
LukashevV <valikallstars@gmail.com>
2021-06-25 20:03:56 +0300

    lesson 44

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -78,16 +78,18 @@ window.addEventListener('DOMContentLoaded', () => {
     const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.toggle('hide');
-            document.body.style.overflow = 'hidden';
-        });
-    });
+    function openModal() {
+        modal.classList.toggle('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
     function closeModal() {
         modal.classList.toggle('hide');
         document.body.style.overflow = '';
     }
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
     modalCloseBtn.addEventListener('click', closeModal);
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
@@ -99,4 +101,12 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+    const modalTimerId = setTimeout(openModal, 5000);
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
 });
