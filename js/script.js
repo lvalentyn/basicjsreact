16092823355a3816966a0e98366a5c37b14f88c4
LukashevV <valikallstars@gmail.com>
2021-06-25 21:19:07 +0300

    lesson 45

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 'use strict';
-let start = new Date();
-for (let i = 0; i < 100000; i++) {
-    let some = i ** 3;
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function () {
+        console.log(`Hello ${this.name}`)
+    }
 }
-let end = new Date();
-
-alert(`Cycle has been ${end - start} ms`)
-// const btn = document.querySelector('.btn');
-
-// function myAnimation() {
-//     const elem = document.querySelector('.box');
-//     let pos = 0;
-//     const id = setInterval(frame, 10);
-//     function frame() {
-//         if (pos === 300) {
-//             clearInterval(id);
-//         } else {
-//             pos++;
-//             elem.style.top = pos + 'px';
-//             elem.style.left = pos + 'px';
-//         }
-//     }
-// }
-// btn.addEventListener('click', myAnimation);
+User.prototype.exit = function () {
+    console.log(`User ${this.name} was exit`);
+}
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+ivan.exit();
+alex.exit();
+ivan.hello();
+alex.hello();
+console.log(ivan);
+console.log(alex);
