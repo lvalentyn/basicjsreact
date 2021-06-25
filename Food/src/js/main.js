80a3430da35d7252d01eb9c4c66d702daadcfc5f
LukashevV <valikallstars@gmail.com>
2021-06-25 19:49:07 +0300

    lesson 43

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -74,4 +74,29 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     setClock('.timer', deadline);
+    // modal
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.toggle('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    function closeModal() {
+        modal.classList.toggle('hide');
+        document.body.style.overflow = '';
+    }
+    modalCloseBtn.addEventListener('click', closeModal);
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && !modal.classList.contains('hide')) {
+            closeModal();
+        }
+    });
 });
